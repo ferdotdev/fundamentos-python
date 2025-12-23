7f255624b8f47ec81832fa61c29b314a64c88ee8
Fernando Emilio Diaz Lopez <fernandoemiliolopez13@gmail.com>
2025-12-23 15:05:32 -0600

    ✨ Add comparison and logical operators examples

---
+++ 17 -  Operadores lógicos OR y NOT con booleanos.py
@@ -0,0 +1,18 @@
+# Operadores de comparacion
+
+x = 5
+y = 3
+
+
+print(x == y)  # Igual a
+print(x != y)  # Diferente de
+print(x > y)   # Mayor que
+print(x < y)   # Menor que
+print(x >= y)  # Mayor o igual que
+print(x <= y)  # Menor o igual que
+
+# Operadores logicos and, or y not
+
+print(x > 2 and y < 5)  # AND: Verdadero si ambas condiciones son verdaderas
+print(x > 2 or y > 5)   # OR: Verdadero si alguna de las condiciones es verdadera
+print(not(x > 2))       # NOT: Invierte el valor booleano de la condicion
