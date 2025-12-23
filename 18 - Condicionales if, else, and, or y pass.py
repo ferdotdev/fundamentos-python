0238d4c688e93380b391f08475c4eb4614aa75c6
Fernando Emilio Diaz Lopez <fernandoemiliolopez13@gmail.com>
2025-12-23 15:47:33 -0600

    ✨ Update documentation in 'Condicionales if, else, and, or y pass.py' to clarify conditional structures and usage of 'pass'

--- 18 - Condicionales if, else, and, or y pass.py
+++ 18 - Condicionales if, else, and, or y pass.py
@@ -1,10 +1,24 @@
+"""
+Condicionales if, else, and, or y pass
+
+En este ejemplo, se muestran diferentes estructuras condicionales, combinadas
+con el uso de operadores logicos, para controlar la ejecucion del codigo.
+
+if es la primera condicion que se evalua. Si es verdadera, se ejecuta el codigo
+Si es falsa, se pasa a la siguiente o siguientes condiciones elif (else if) 
+Si ninguna de las condiciones if o elif es verdadera, se ejecuta el codigo dentro del bloque else.
+
+Ademas, se utiliza la palabra reservada 'pass' para indicar que no se realiza
+ninguna accion en un bloque condicional.
+"""
+
 a = 10
 b = 5
 c = 10
 
 if a < b and a == c:
     print("a es menor que b y a es igual a c")
-elif a < b or b < c:
+elif a < b or b < c: 
     print("Al menos una de las condiciones es verdadera")
 else:
     print("Condicion no cumplida")
