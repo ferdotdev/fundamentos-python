1df78bb39a637dccffc96fcb234e705a0a346e11
Fernando Emilio Diaz Lopez <fernandoemiliolopez13@gmail.com>
2025-12-23 12:59:16 -0600

    ✨ Add examples of assignment operators and explain expressions vs statements

---
+++ 16 - Operadores de asignacion.py
@@ -0,0 +1,20 @@
+# Operadores de asignacion
+
+
+x = 100    # Asignacion inicial
+x = x + 3  # Asignacion simple
+x += 3     # Asignacion con operador de suma
+x -= 2     # Asignacion con operador de resta
+x *= 4     # Asignacion con operador de multiplicacion
+x /= 2     # Asignacion con operador de division
+x %= 3     # Asignacion con operador de modulo
+print(x)
+
+y = 5
+y //= 2    # Asignacion con operador de division entera
+y **= 3    # Asignacion con operador de potencia
+
+# Operador walrus
+print(y := 2) 
+
+print(y)
