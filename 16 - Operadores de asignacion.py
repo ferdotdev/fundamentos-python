798c6309c27fee139072b48c2ef6430f4ad98f57
Fernando <fernandoemiliolopez13@gmail.com>
2026-01-08 17:11:15 -0600

    ✨ Refactor assignment operators section for improved clarity and consistency in examples

--- 16 - Operadores de asignacion.py
+++ 16 - Operadores de asignacion.py
@@ -3,17 +3,23 @@ Operadores de asignacion
 En Python, los operadores de asignación se utilizan para asignar valores a las variables.
 """
 
-x = 100    # Asignacion inicial
-x = x + 3  # Asignacion simple
-x += 3     # Asignacion con operador de suma
-x -= 2     # Asignacion con operador de resta
-x *= 4     # Asignacion con operador de multiplicacion
-x /= 2     # Asignacion con operador de division
-x %= 3     # Asignacion con operador de modulo
+x = 100    # Asignacion de valores simple
+x = x + 3  # Asignacion de variables simple
 
+"""
+Operadores de asignacion
+
+Los operadores de asignación pueden usarse con numeros o con otras variables
+"""
+x += z     # Operador de asignacion con otra variable
+x += 3     # Operador de asignacion de suma
+x -= 2     # Operador de asignacion de resta
+x *= 4     # Operador de asignacion de multiplicacion
+x /= 2     # Operador de asignacion de division
+x %= 3     # Operador de asignacion de modulo
 y = 5
-y //= 2    # Asignacion con operador de division entera
-y **= 3    # Asignacion con operador de potencia
+y //= 2    # Operador de asignacion de division entera
+y **= 3    # Operador de asignacion de potencia
 
 """
 Operador walrus
@@ -22,4 +28,5 @@ Es el operador de asignación de expresión (también conocido como
 el "operador walrus" :=), que permite asignar valores a las variables como parte
 de una expresión.
 """
+
 print(y := 2) 
